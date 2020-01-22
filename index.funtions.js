9a1337397b765eab0f6a9e2b35c3a19ca12b865d
Byronpe <59664668+Byronpe@users.noreply.github.com>
2020-01-22 10:19:11 -0700

    Add files via upload

---
+++ index.funtions.js
@@ -0,0 +1,27 @@
+console.log("helloworld");
+
+
+function multiply(number1, number2){
+    console.log(number1 * number2);
+
+}
+multiply(6, 7);
+
+function multiply(numb1, numb2){
+    console.log(numb1 * numb2);
+
+}
+
+multiply(9, 9);
+
+function substract(number1, number2){
+    console.log(number1 - number2);
+
+}
+substract(10, 11);
+
+function addition(num1, numb2){
+    console.log(num1 + numb2);
+
+}
+addition(100, 200)
