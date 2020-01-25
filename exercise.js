1afb7526607d32e786b0984d768ce4e94a406da4
Byronpe <59664668+Byronpe@users.noreply.github.com>
2020-01-25 07:48:57 -0700

    Add files via upload

---
+++ exercise.js
@@ -0,0 +1,33 @@
+function sum(num1, num2){
+    console.log(num1 + num2)
+}
+sum(100,200);
+
+function divide(number1, number2){
+    return number1 / number2
+}
+console.log(divide(500,250));
+
+function subtraction(n1, n2, n3){
+    console.log(n1 - n2 - n3)
+}
+subtraction(100,50,49)
+
+function friendship(person1, person2){
+    const friends = person1 + " is friends with " + person2 + " forever and always";
+    console.log(friends)
+}
+friendship("Freddy", "Teena");
+
+const bird = {
+    type: "bald", animal: "eagle"
+}
+console.log(bird.type + " " + bird.animal);
+
+const first = function(){
+    const name = "Xavier";
+    return name;
+}
+console.log(first());
+
+
